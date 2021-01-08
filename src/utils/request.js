42d07caa218b81e62a4b06277851486c6dcdeec5
janny <2431349655@qq.com>
2021-01-08 16:50:20 +0800

    在当前页面显示导航条加载动画

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,11 @@
 const baseURL = "https://api-hmugo-web.itheima.net/api/public/v1"
 // ...config 接收所有参数，解构出 url
+let requestCount = 0;
 const request = ({url,...config})=>{
+  
     return new Promise((resolve,reject)=>{
+        requestCount++;
+        uni.showNavigationBarLoading()
         uni.request({
             // 展开运算符把所有请求参数展开作为 request 的参数
             ...config,
@@ -12,6 +16,12 @@ const request = ({url,...config})=>{
             },
             fail:(err)=>{
                 reject(err)
+            },
+            complete:()=>{
+                requestCount--;
+                if(requestCount === 0){
+                    uni.hideNavigationBarLoading()
+                }
             }
         });
     })
